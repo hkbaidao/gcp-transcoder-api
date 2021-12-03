05935b9b943174eb732b6c0d2025b0ca7e5a7fc8
Top Chan <51317683+Top-Chan@users.noreply.github.com>
2021-12-03 09:25:27 +0800

    Update create_job_no_sub.py

--- create_job/create_job_no_sub.py
+++ create_job/create_job_no_sub.py
@@ -8,18 +8,18 @@ def start():
 
     token_command = "gcloud auth application-default print-access-token"
     ret, val = subprocess.getstatusoutput(token_command)
-    PROJECT_ID = "jianquan-test"
-    location = "asia-east1"
+    PROJECT_ID = "project-id"
+    location = "location"
 
-    input_uri = "\"gs://test-bucket-input/49_1_001.mkv\""
-    output_uri = "\"gs://test-bucket-outputfile/49_1_001/\""
+    input_uri = "\"gs://bucket-name/object-name\""
+    output_uri = "\"gs://bucket-name/folder-name/\""
     track = "1"
 
     command = "curl -X POST \"https://transcoder.googleapis.com/v1/projects/" +PROJECT_ID+ "/locations/" +location+ "/jobs\" " + \
               "-H \"Authorization: Bearer \"" + val + \
               " -H \"Content-Type: application/json; charset=utf-8\"" + \
               " -d" + \
-              " \'{ \"config\": { \"inputs\": [ { \"key\": \"input0\", \"uri\":" +input_uri+"}], \"editList\": [ { \"key\": \"atom1\", \"inputs\": [ \"input0\", ] } ], \"elementaryStreams\": [ { \"key\": \"video-stream0\", \"videoStream\": { \"h264\":{ \"heightPixels\": 360, \"widthPixels\": 640, \"bitrateBps\": 55000, \"frameRate\": 60 }} }, { \"key\": \"audio-stream0\", \"audioStream\": { \"codec\": \"aac\", \"bitrateBps\": 64000, \"channelCount\": 2, \"mapping\": [ { \"atomKey\": \"atom1\", \"inputKey\": \"input0\", \"inputTrack\": "+track+", \"inputChannel\": 0, \"outputChannel\": 0 },{ \"atomKey\": \"atom1\", \"inputKey\": \"input0\", \"inputTrack\": "+track+", \"inputChannel\": 1, \"outputChannel\": 1} ]} } ], \"muxStreams\": [ { \"key\": \"sd\", \"container\": \"ts\", \"elementaryStreams\": [ \"video-stream0\", \"audio-stream0\" ], \"segmentSettings\": { \"segmentDuration\": \"6s\", \"individualSegments\": true } }, ], \"manifests\": [ { \"fileName\": \"master.m3u8\", \"type\": \"HLS\", \"muxStreams\": [ \"sd\", ] } ], \"output\": { \"uri\": " +output_uri+ "}, \"pubsub_destination\": { \"topic\": \"projects/jianquan-test/topics/test-transcoder\"} } }\'"
+              " \'{ \"config\": { \"inputs\": [ { \"key\": \"input0\", \"uri\":" +input_uri+"}], \"editList\": [ { \"key\": \"atom1\", \"inputs\": [ \"input0\", ] } ], \"elementaryStreams\": [ { \"key\": \"video-stream0\", \"videoStream\": { \"h264\":{ \"heightPixels\": 360, \"widthPixels\": 640, \"bitrateBps\": 55000, \"frameRate\": 60 }} }, { \"key\": \"audio-stream0\", \"audioStream\": { \"codec\": \"aac\", \"bitrateBps\": 64000, \"channelCount\": 2, \"mapping\": [ { \"atomKey\": \"atom1\", \"inputKey\": \"input0\", \"inputTrack\": "+track+", \"inputChannel\": 0, \"outputChannel\": 0 },{ \"atomKey\": \"atom1\", \"inputKey\": \"input0\", \"inputTrack\": "+track+", \"inputChannel\": 1, \"outputChannel\": 1} ]} } ], \"muxStreams\": [ { \"key\": \"sd\", \"container\": \"ts\", \"elementaryStreams\": [ \"video-stream0\", \"audio-stream0\" ], \"segmentSettings\": { \"segmentDuration\": \"6s\", \"individualSegments\": true } }, ], \"manifests\": [ { \"fileName\": \"master.m3u8\", \"type\": \"HLS\", \"muxStreams\": [ \"sd\", ] } ], \"output\": { \"uri\": " +output_uri+ "}, \"pubsub_destination\": { \"topic\": \"projects/project-id/topics/topic-name\"} } }\'"
 
     print("    " + command)
 
